d1a51388c48ccc6b9117baad5a7ecd6bc63bc4c0
Tashika Cruz <sebastiantashika@gmail.com>
2023-02-21 23:12:35 +0900

    animated some parts of navbar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Work from "./component/work/Work"
 import Project from "./component/project/Project"
 import Contact from "./component/contact/Contact"
 import "./app.scss"
+import { useState } from "react"
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <div className="app">
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Home />
         <About />
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,20 +1,20 @@
 import './navbar.scss'
-import Logo from '../../assets/images/TC-logo-light.png'
+import {} from "@material-ui/icons"
+// import Logo from '../../assets/images/TC-logo-light.png'
 
-export default function Navbar() {
+export default function Navbar({ menuOpen, setMenuOpen }) {
   return(
-    <div className="navbar">
+    <div className={"navbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#home" className="logo">
-            <img src={Logo} alt="tashika" />
-          </a>
-          <div className="itemContainer">
-
-          </div>
+          <a href="#home" className="logo">TC</a>
         </div>
         <div className="right">
-
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
